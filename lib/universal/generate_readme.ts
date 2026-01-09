4ad3f65ccd994fa913f6cc02c177e5aed59a150a
Siju Joseph <siju-joseph@netspective.in>
2026-01-09 12:07:12 +0530

    fix: format

--- lib/universal/generate_readme.ts
+++ lib/universal/generate_readme.ts
@@ -1,50 +1,51 @@
-
 import { MarkdownDoc } from "./fluent-md.ts";
 
 const md = new MarkdownDoc();
 md.h1("@spry/universal");
-md.p("Universal utilities for Spry core, compatible with Deno and other environments.");
+md.p(
+  "Universal utilities for Spry core, compatible with Deno and other environments.",
+);
 
 md.h2("Modules");
 md.ul(
-    "annotations",
-    "code-comments",
-    "code",
-    "collectable",
-    "depends",
-    "directive",
-    "doctor",
-    "event-bus",
-    "flexible-interpolator",
-    "flexible-pattern",
-    "fluent-md",
-    "gitignore",
-    "json-stringify-aide",
-    "lister-tabular-tui",
-    "lister-tree-tui",
-    "os-user",
-    "path-tree-tabular",
-    "path-tree",
-    "pmd-shebang",
-    "posix-pi",
-    "render",
-    "resource-contributions",
-    "resource",
-    "reverse-proxy-simulate",
-    "route",
-    "sql-text",
-    "tabular-json",
-    "task-visuals",
-    "task",
-    "text-utils",
-    "tmpl-literal-aide",
-    "unsafe-js-expr",
-    "version",
-    "watcher",
-    "zod-aide"
+  "annotations",
+  "code-comments",
+  "code",
+  "collectable",
+  "depends",
+  "directive",
+  "doctor",
+  "event-bus",
+  "flexible-interpolator",
+  "flexible-pattern",
+  "fluent-md",
+  "gitignore",
+  "json-stringify-aide",
+  "lister-tabular-tui",
+  "lister-tree-tui",
+  "os-user",
+  "path-tree-tabular",
+  "path-tree",
+  "pmd-shebang",
+  "posix-pi",
+  "render",
+  "resource-contributions",
+  "resource",
+  "reverse-proxy-simulate",
+  "route",
+  "sql-text",
+  "tabular-json",
+  "task-visuals",
+  "task",
+  "text-utils",
+  "tmpl-literal-aide",
+  "unsafe-js-expr",
+  "version",
+  "watcher",
+  "zod-aide",
 );
 
 // Output the markdown
 if (import.meta.main) {
-    console.log(md.write());
+  console.log(md.write());
 }
